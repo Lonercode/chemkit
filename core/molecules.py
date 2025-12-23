56702d76043bf783833f02f3a8582304edfca4ed
Lonercode <penpalclassmate@gmail.com>
2025-12-23 12:54:00 +0100

    feat: function to return molecular formula and updates to view_molecule function

--- core/molecules.py
+++ core/molecules.py
@@ -12,9 +12,14 @@ Example
 >>> image3d = view_molecule(smile, "3d")
 """
 
+import webbrowser
+
 import py3Dmol
 from rdkit import Chem
 from rdkit.Chem import Draw
+from rdkit.Chem.rdMolDescriptors import CalcMolFormula
+
+# ToDo: Work on symbols and metadata for molecule.
 
 
 def view_molecule(dim: str, smiles: str = None, file: str = None):
@@ -32,33 +37,22 @@ def view_molecule(dim: str, smiles: str = None, file: str = None):
         view = py3Dmol.view(width=800, height=800)
         view.addModel(Chem.MolToMolBlock(mol), "mol")
         view.setStyle({"stick": {}})
-        for atom in mol.GetAtoms():
-            idx = atom.GetIdx()
-            symbol = atom.GetSymbol()
-            view.addLabel(
-                symbol,
-                {
-                    "position": {
-                        "x": mol.GetConformer().GetAtomPosition(idx).x,
-                        "y": mol.GetConformer().GetAtomPosition(idx).y,
-                        "z": mol.GetConformer().GetAtomPosition(idx).z,
-                    },
-                    "backgroundColor": "white",
-                    "fontColor": "black",
-                    "fontSize": 25,
-                    "showBackground": False,
-                    "fontOpacity": 1,
-                },
-            )
-        view.setViewStyle(
-            {
-                "style": "outline",
-                "color": "white",
-                "width": 0.05,
-            }
-        )
         view.zoomTo()
         with open("molecule.html", "w") as f:
             f.write(view._make_html())
+        webbrowser.open_new("molecule.html")
     else:
-        print("Invalid molecule provided")
+        raise ValueError("Invalid molecule provided")
+
+
+def get_molecular_formula(smiles: str = None, file: str = None):
+    if file is None:
+        mol = Chem.MolFromSmiles(smiles)
+    else:
+        mol = Chem.MolFromMolFile(file)
+
+    formula = CalcMolFormula(mol)
+    return formula
+
+
+__all__ = ["view_molecule", "get_molecular_formula"]
